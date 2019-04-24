d170e37257f878ae7454c5a5bc1194a17b5bff1e
laura-mso <laura.socha@gmx.net>
2019-04-24 16:49:31 +0200

    Random simpson quotes added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import "./App.css";
 import Button from "./components/Button";
 import Location from "./components/travellocation";
 import SimpsonsAPI from "./components/SimpsonsAPI";
+import GenerateQuote from "./components/GenerateQuote";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: {}
+    };
+    this.selectQuote = this.selectQuote.bind(this);
+  }
+
+  selectQuote() {
+    fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          quotes: json[0]
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.selectQuote();
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,7 +36,8 @@ class App extends Component {
         <div className="Travelsection">
           <Location />
         </div>
-        <SimpsonsAPI />
+        <SimpsonsAPI quotes={this.state.quotes} />
+        <GenerateQuote getQuote={this.selectQuote} />
       </div>
     );
   }
